9898bc49fd0eda161e6761723988451d451f5f74
Thomas Jonstrup <thomas_jonstrup@hotmail.com>
2025-07-27 20:53:48 +0200

    Update Section.tsx

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,11 +1,11 @@
 import { cn } from "@/lib/utils";
 
 interface SectionProps extends React.PropsWithChildren {
-	title: string;
+	title?: string;
 	className?: string;
 }
 
-export const Section = ({ title, className, children }: SectionProps) => {
+export const Section = ({ className, children }: SectionProps) => {
 	return (
 		<section className={cn("container mx-auto px-4 py-20", className)}>
 			{children}
