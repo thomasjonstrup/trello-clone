a05fe36a4e127d9bc13b2ef25b02d7921cf8d871
Thomas Jonstrup <thomas_jonstrup@hotmail.com>
2025-07-27 18:03:17 +0200

    feat: Added gsap and header nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,33 +1,45 @@
-import { Link } from '@tanstack/react-router'
+import { Link } from "@tanstack/react-router";
+import { LucideTrello } from "lucide-react";
 
 export default function Header() {
-  return (
-    <header className="p-2 flex gap-2 bg-white text-black justify-between">
-      <nav className="flex flex-row">
-        <div className="px-2 font-bold">
-          <Link to="/">Home</Link>
-        </div>
+	return (
+		<header className="border-b bg-white/80 backdrop-blur-sm sticky top-0 z-50">
+			<div className="container mx-auto px-4 py-3 sm:py-4 flex items-center justify-between">
+				<div className="flex items-center space-x-2">
+					<a href="/" className="flex items-center space-x-2">
+						<LucideTrello />
+						<span className="text-xl sm:text-2xl font-bold text-gray-900">
+							Trello Clone
+						</span>
+					</a>
+				</div>
 
-        <div className="px-2 font-bold">
-          <Link to="/demo/store">Store</Link>
-        </div>
+				<nav className="flex flex-row">
+					<div className="px-2 font-bold">
+						<Link to="/">Home</Link>
+					</div>
 
-        <div className="px-2 font-bold">
-          <Link to="/demo/tanstack-query">TanStack Query</Link>
-        </div>
+					<div className="px-2 font-bold">
+						<Link to="/demo/store">Store</Link>
+					</div>
 
-        <div className="px-2 font-bold">
-          <Link to="/demo/form/simple">Simple Form</Link>
-        </div>
+					<div className="px-2 font-bold">
+						<Link to="/demo/tanstack-query">TanStack Query</Link>
+					</div>
 
-        <div className="px-2 font-bold">
-          <Link to="/demo/form/address">Address Form</Link>
-        </div>
+					<div className="px-2 font-bold">
+						<Link to="/demo/form/simple">Simple Form</Link>
+					</div>
 
-        <div className="px-2 font-bold">
-          <Link to="/dashboard">Dashboard</Link>
-        </div>
-      </nav>
-    </header>
-  )
+					<div className="px-2 font-bold">
+						<Link to="/demo/form/address">Address Form</Link>
+					</div>
+
+					<div className="px-2 font-bold">
+						<Link to="/dashboard">Dashboard</Link>
+					</div>
+				</nav>
+			</div>
+		</header>
+	);
 }
